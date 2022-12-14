f8046ba0d354c4bfef79b0239b6253b0d5be630c
Heetaek Woo <hiiteq@gmail.com>
2022-12-14 16:58:19 +0900

    functionize font weight checker, artwork update

--- code.ts
+++ code.ts
@@ -22,29 +22,43 @@ Promise.all([font400, font100, font200, font300, font500, font600, font700, font
     // 3. 각 text 요소의 font-weight를 체크하고 그에 맞는 Pretendard Variable 폰트로 지정합니다.
     for (const textNode of textNodes) {
 
+      // textNode의 fontWeight가 figma.mixed일 경우, 아래에 cssWeight로 처리하는것을 무시하고 fontWeight를 출력한다.
+      if (textNode.fontWeight === figma.mixed) {
+        // textNode에 담긴 characters의 font-family와 font-weight를 추출하여, array로 만든다.
+        const textNodeFont = textNode.characters.split(' ').map((item) => {
+          return item.split('/')[0]
+        })
+        
+        continue
+      }
+
       const cssWeight = Number(textNode.fontWeight)
 
-      textNode.fontName = { family: "Pretendard Variable", style: "Thin" }
-      if (cssWeight <= 149) {
-        textNode.fontName = { family: "Pretendard Variable", style: "Thin" }
-      } else if (cssWeight >= 150 && cssWeight < 250) {
-        textNode.fontName = { family: "Pretendard Variable", style: "ExtraLight" }
-      } else if (cssWeight >= 250 && cssWeight < 350) {
-        textNode.fontName = { family: "Pretendard Variable", style: "Light" }
-      } else if (cssWeight >= 350 && cssWeight < 450) {
-        textNode.fontName = { family: "Pretendard Variable", style: "Regular" }
-      } else if (cssWeight >= 450 && cssWeight < 550) {
-        textNode.fontName = { family: "Pretendard Variable", style: "Medium" }
-      } else if (cssWeight >= 550 && cssWeight < 650) {
-        textNode.fontName = { family: "Pretendard Variable", style: "SemiBold" }
-      } else if (cssWeight >= 650 && cssWeight < 750) {
-        textNode.fontName = { family: "Pretendard Variable", style: "Bold" }
-      } else if (cssWeight >= 750 && cssWeight < 850) {
-        textNode.fontName = { family: "Pretendard Variable", style: "ExtraBold" }
-      } else if (cssWeight >= 850) {
-        textNode.fontName = { family: "Pretendard Variable", style: "Black" }
-      }
+      setFontName(textNode, cssWeight)
+
     }
     // 4. 작업이 완료되면 플러그인을 종료합니다.
     figma.closePlugin()
   })
+
+  function setFontName (textNode: { type: "TEXT" } & TextNode, cssWeight: number) {
+    if (cssWeight <= 149) {
+      textNode.fontName = { family: "Pretendard Variable", style: "Thin" }
+    } else if (cssWeight >= 150 && cssWeight < 250) {
+      textNode.fontName = { family: "Pretendard Variable", style: "ExtraLight" }
+    } else if (cssWeight >= 250 && cssWeight < 350) {
+      textNode.fontName = { family: "Pretendard Variable", style: "Light" }
+    } else if (cssWeight >= 350 && cssWeight < 450) {
+      textNode.fontName = { family: "Pretendard Variable", style: "Regular" }
+    } else if (cssWeight >= 450 && cssWeight < 550) {
+      textNode.fontName = { family: "Pretendard Variable", style: "Medium" }
+    } else if (cssWeight >= 550 && cssWeight < 650) {
+      textNode.fontName = { family: "Pretendard Variable", style: "SemiBold" }
+    } else if (cssWeight >= 650 && cssWeight < 750) {
+      textNode.fontName = { family: "Pretendard Variable", style: "Bold" }
+    } else if (cssWeight >= 750 && cssWeight < 850) {
+      textNode.fontName = { family: "Pretendard Variable", style: "ExtraBold" }
+    } else if (cssWeight >= 850) {
+      textNode.fontName = { family: "Pretendard Variable", style: "Black" }
+    }
+  }
