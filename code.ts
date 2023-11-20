a01b1d3525fb4b6c5b5d3b0db0647978e2edef00
Heetaek Woo <hiiteq@gmail.com>
2023-11-20 14:59:43 +0900

    1.0.7
    - 폰트 변수화.
    - Variable이 아닌 일반 폰트로 변경

--- code.ts
+++ code.ts
@@ -1,48 +1,44 @@
-// 아래의 순서대로 작동하는 figma의 Run Once 타입 플러그인 용 ts 코드를 작성해 줘.
-// 1. 현재 page에서 `Pretendard Variable`의 모든 폰트가 로드되는 것을 기다립니다.
-// 2. 현재 page에 있는 모든 text 요소를 가져옵니다.
-// 3. 각 text 요소의 fontWeight의 값(예시:100~900)을 체크하고, fontWeight가 149이하는 “Pretendard Variable”폰트의 Thin, 150이상 250미만은 “Pretendard Variable”폰트의 ExtraLight, 250 이상 350미만은 “Pretendard Variable”폰트의 Light, 350이상 450미만은 “Pretendard Variable”폰트의 Regular, 450이상 550미만은 “Pretendard Variable”폰트의 Medium, 550이상 650미만은 “Pretendard Variable”폰트의 SemiBold, 650이상 750미만은 “Pretendard Variable”폰트의 Bold, 750이상 850미만은 “Pretendard Variable”폰트의 ExtraBold, 850이상은 “Pretendard Variable”폰트의 Black으로 지정한다.
-// 4. 작업이 완료되면 플러그인을 종료합니다.
-
-// 1. 현재 page에서 `Pretendard Variable`의 모든 폰트가 로드되는 것을 기다립니다.
-// font400이 1000ms 이내에 로드되지 않으면 에러를 띄웁니다.
+const fontName = 'Pretendard'; // Change this to use a different font
 
 const loadFonts = async () => {
   console.log(`Awaiting the fonts.`)
-  await figma.loadFontAsync({ family: `Pretendard Variable`, style: `Regular` })
-  await figma.loadFontAsync({ family: `Pretendard Variable`, style: `Thin` })
-  await figma.loadFontAsync({ family: `Pretendard Variable`, style: `ExtraLight` })
-  await figma.loadFontAsync({ family: `Pretendard Variable`, style: `Light` })
-  await figma.loadFontAsync({ family: `Pretendard Variable`, style: `Medium` })
-  await figma.loadFontAsync({ family: `Pretendard Variable`, style: `SemiBold` })
-  await figma.loadFontAsync({ family: `Pretendard Variable`, style: `Bold` })
-  await figma.loadFontAsync({ family: `Pretendard Variable`, style: `ExtraBold` })
-  await figma.loadFontAsync({ family: `Pretendard Variable`, style: `Black` })
+  const fontStyles = ['Regular', 'Thin', 'ExtraLight', 'Light', 'Medium', 'SemiBold', 'Bold', 'ExtraBold', 'Black'];
+  const results = await Promise.allSettled(fontStyles.map(style => figma.loadFontAsync({ family: fontName, style })));
+
+  results.forEach((result: PromiseSettledResult<any>, i: number) => {
+    if (result.status === 'rejected' && typeof result.reason === 'string') {
+      console.log(`Failed to load ${fontStyles[i]}: ${result.reason}`);
+    }
+  });
 }
 
 loadFonts()
-  .then(() => {
+  .then(async () => {
     console.log(`Fonts loaded.`)
-    figma.notify(`Change all the text layers on the current page to Pretendard Variable.`, { timeout: 500})
+    figma.notify(`Change all the text layers on the current page to ${fontName}.`, { timeout: 500})
 
-    // 2. 현재 page에 있는 모든 text 요소를 가져옵니다.
     const textNodes = figma.currentPage.findAllWithCriteria({ types: ['TEXT'] })
+    if (textNodes.length === 0) {
+      console.log(`No text nodes found on the current page.`)
+      return
+    }
     let count = 0
     let ignored = 0
-    // 처리해야할 레이어의 수를 textNodes.length로 알려줍니다.
-    // onDequeue되는 경우 즉시 종료되는 것을 방지하기 위해 timeout을 0으로 설정합니다.
     figma.notify(`Processing ${textNodes.length} layers...`, { timeout: 500 })
 
-    // 3. 각 text 요소의 font-weight를 체크하고 그에 맞는 Pretendard Variable 폰트로 지정합니다.
     for (const textNode of textNodes) {
 
-      // textNode의 fontWeight가 figma.mixed일 경우, 아래의 if문 내부처리를 따른다.
+      if (textNode.hasMissingFont) {
+        console.log(`Text node has missing font: ${textNode.id}`);
+        continue;
+      }
+
       if (textNode.fontWeight === figma.mixed) {
-        // textNode에 담긴 한 글자마다, fontWeight를 checkFontWeight()를 실행하고, return되는 String 값을 그 글자의 fontName의 style로 지정한다.
         for (let i = 0; i < textNode.characters.length; i++) {
           const charWeight = Number(textNode.getRangeFontWeight(i, i+1))
           console.log(charWeight)
-          textNode.setRangeFontName(i, i+1, { family: `Pretendard Variable`, style: String(checkFontWeight(charWeight)) })
+          await figma.loadFontAsync({ family: fontName, style: String(getFontStyle(charWeight)) })
+          textNode.setRangeFontName(i, i+1, { family: fontName, style: String(getFontStyle(charWeight)) })
         }
         
         count++
@@ -51,23 +47,21 @@ loadFonts()
       }
 
       const cssWeight = Number(textNode.fontWeight)
-      //family에 `Pretendard Variable`, style에는 checkFontWeight의 string 리턴값을 넣어준다.
-      textNode.fontName = { family: `Pretendard Variable`, style: String(checkFontWeight(cssWeight)) }
+      await figma.loadFontAsync({ family: fontName, style: String(getFontStyle(cssWeight)) })
+      textNode.fontName = { family: fontName, style: String(getFontStyle(cssWeight)) }
 
       count++
 
     }
-    // 4. 작업이 완료되면 작업이 끝났음을 notify하고 플러그인을 종료합니다.
     console.log(`All done.`)
     figma.closePlugin(`✅ All ${count} text layers have been updated.`)
   })
   .catch((error) => {
     console.log(error)
-    figma.closePlugin(`❌ Failed to load fonts. Please check if the Pretendard Variable is installed and try again.`)
-    
+    figma.closePlugin(`❌ Failed to load fonts. Please check if the ${fontName} is installed and try again.`)
   })
 
-function checkFontWeight(cssWeight: number) {
+function getFontStyle(cssWeight: number) {
   if (cssWeight <= 149) {
     return `Thin`
   } else if (cssWeight >= 150 && cssWeight < 250) {
@@ -86,5 +80,7 @@ function checkFontWeight(cssWeight: number) {
     return `ExtraBold`
   } else if (cssWeight >= 850) {
     return `Black`
+  } else {
+    return `Regular`
   }
 }
